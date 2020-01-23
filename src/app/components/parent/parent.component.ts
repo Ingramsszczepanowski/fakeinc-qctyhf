e29269526749fa4587aa26dbb4e7b6f06a517f1c
Ingramsszczepanowski <steven.szczepanowski@ingrammicro.com>
2020-01-23 13:48:26 -0500

    1 file modified

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -9,6 +9,7 @@ import { SearchFilterPipe} from '../../Pipes/search-filter.pipe';
 export class ParentComponent implements OnInit {
   employees: any[];
   headers = ["Code","Name","Gender","Salary","Date of Birth"];
+  SearchItemCount: any;
   constructor() {}
 
   ngOnInit(){
@@ -23,5 +24,12 @@ export class ParentComponent implements OnInit {
       { code: 'emp03', name: 'Sandesh', gender: 'Male', salary: 90000, dob: '4/4/1991'},
       { code: 'emp04', name: 'Sarah', gender: 'Female', salary: 80000, dob: '5/5/1995'}
     ]
+  }
+
+
+  // Need to get count of items returned by search filter anywhere inside this component
+  getFilterItemCount()
+  {
+    
   }
 }
